cf701f6fed765df66ea66d2ad9113bead8259650
rahul sahay <rahulsahay19@gmail.com>
2015-09-17 16:58:10 +0530

    3rd build

---
+++ ExtensionMethods/ExceptionExtension/ExceptionExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods.ExceptionExtension
+{
+   public static class ExceptionExtension
+    {
+       public static string CompleteMessage(this Exception ex)
+       {
+           var builder = new StringBuilder();
+           while (ex != null)
+           {
+               builder.AppendFormat("{0}{1}", ex.Message, Environment.NewLine);
+               ex = ex.InnerException;
+           }
+           return builder.ToString();
+       }
+    }
+}
---
+++ Legacy.Tests/Extension-Tests/ExceptionExtensionTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using ExtensionMethods.ExceptionExtension;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Legacy.Tests.Extension_Tests
+{
+    [TestClass]
+    public class ExceptionExtensionTests
+    {
+        [TestMethod]
+        public void DivideBy1()
+        {
+            Assert.AreEqual(7, Divide(7, 1));
+        }
+        [TestMethod]
+        public void DivideBy0()
+        {
+            try
+            {
+                Divide(7, 0);
+                Assert.Fail("Should throw exception");
+            }
+            catch (Exception ex)
+            {
+                //Debug.WriteLine(ex.Message);
+                //Custom Message
+                Debug.WriteLine(ex.CompleteMessage());
+                Assert.IsInstanceOfType(ex,typeof(ApplicationException));
+            }
+        }
+
+        public double Divide(int num, int denom)
+        {
+            try
+            {
+                return num/denom;
+            }
+            catch (Exception ex)
+            {
+                var invalidOp = new InvalidOperationException("Invalid Opeartion", ex);
+                var message = string.Format("Divison Failed, numerator:{0}-denominator:{1}", num, denom);
+                throw new ApplicationException(message,invalidOp);
+            }
+        }
+    }
+}
